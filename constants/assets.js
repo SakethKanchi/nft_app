ce1f1a481da45d541b74a413efdbaf56a97c2a5b
SakethKanchi <sakethkanchi3@gmail.com>
2022-04-02 00:25:21 +0530

    initial commit

---
+++ constants/assets.js
@@ -0,0 +1,41 @@
+import badge from "../assets/icons/badge.png";
+import eth from "../assets/icons/eth.png";
+import heart from "../assets/icons/heart.png";
+import left from "../assets/icons/left.png";
+import search from "../assets/icons/search.png";
+
+import logo from "../assets/images/logo.png";
+import nft01 from "../assets/images/walls/bench.png";
+import nft02 from "../assets/images/walls/dream.png";
+import nft03 from "../assets/images/walls/city-scape.png";
+import nft04 from "../assets/images/walls/alley.png";
+import nft05 from "../assets/images/walls/spaceship.png";
+import nft06 from "../assets/images/walls/street.png";
+import nft07 from "../assets/images/walls/forest.png";
+
+import person01 from "../assets/images/person01.png";
+import person02 from "../assets/images/person02.png";
+import person03 from "../assets/images/person03.png";
+import person04 from "../assets/images/person04.png";
+
+export default {
+  badge,
+  eth,
+  heart,
+  left,
+  search,
+
+  logo,
+  nft01,
+  nft02,
+  nft03,
+  nft04,
+  nft05,
+  nft06,
+  nft07,
+
+  person01,
+  person02,
+  person03,
+  person04,
+};
---
+++ constants/shoe_assets.js
@@ -0,0 +1,41 @@
+import badge from "../assets/icons/badge.png";
+import money from "../assets/icons/money.png";
+import heart from "../assets/icons/heart.png";
+import left from "../assets/icons/left.png";
+import search from "../assets/icons/search.png";
+
+import logo from "../assets/images/logo.png";
+import shoes1 from "../assets/images/shoes/shoes1.png";
+import shoes2 from "../assets/images/shoes/shoes2.png";
+import shoes3 from "../assets/images/shoes/shoes3.png";
+import shoes4 from "../assets/images/shoes/shoes4.png";
+import shoes5 from "../assets/images/shoes/shoes5.png";
+import shoes6 from "../assets/images/shoes/shoes6.png";
+import shoes7 from "../assets/images/shoes/shoes7.png";
+
+import person01 from "../assets/images/avatar/person1.png";
+import person01 from "../assets/images/avatar/person2.png";
+import person01 from "../assets/images/avatar/person3.png";
+import person01 from "../assets/images/avatar/person4.png";
+
+export default {
+  badge,
+  money,
+  heart,
+  left,
+  search,
+
+  logo,
+  shoes1,
+  shoes2,
+  shoes3,
+  shoes4,
+  shoes5,
+  shoes6,
+  shoes7,
+
+  person01,
+  person02,
+  person03,
+  person04,
+};
